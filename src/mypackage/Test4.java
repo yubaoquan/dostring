20ad208dd1b4b17b3630799a605af7c97d2c96da
yubaoquan <721731428@qq.com>
2014-07-18 10:51:30 +0800

    10

--- src/mypackage/Test4.java
+++ src/mypackage/Test4.java
@@ -1,5 +1,8 @@
 package mypackage;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Test4 {
 
 	public static void method1() {
@@ -16,6 +19,26 @@ public class Test4 {
 		System.out.println("hello");
 	}
 	public static void main(String[] args) {
-		Test4.method1();
+//		Test4.method1();
+		testFindSubStrings();
+	}
+	
+	public static int[] findSubStrings(String input, String subString) {
+		int[] result = new int[input.length()];
+		Pattern p = Pattern.compile(subString);
+		Matcher m = p.matcher(input);
+		int i = 0;
+		while (m.find()) {
+			int position = m.start();
+			result[i ++] = position;
+			System.out.println(position);
+		}
+		return result;
+	}
+	
+	public static void testFindSubStrings() {
+		String input = "yuabcbaoabcquanabc";
+		String subString = "abc";
+		findSubStrings(input, subString);
 	}
 }
